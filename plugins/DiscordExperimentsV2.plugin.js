231e798e52d381de4b45b30fcb6adb02eab1326b
Dylan Saffer <mrscaryspacecat@gmail.com>
2023-04-26 22:58:17 -0400

    Initial commit

---
+++ plugins/DiscordExperimentsV2.plugin.js
@@ -0,0 +1,29 @@
+/**
+ * @name DiscordExperimentsV2
+ * @description Remake of the discordExperiments. Please make sure to exit out of settings everytime you turn this plugin on or off for it to have an effect.
+ * @author MrScarySpaceCat
+ * @version 1.0.0
+ */
+
+
+function setExperiments(value) {
+    webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => cache=Object.values(r.c)]);
+    var UserStore = cache.find(m => m?.exports?.default?.getCurrentUser).exports.default;
+    var actions = Object.values(UserStore._dispatcher._actionHandlers._dependencyGraph.nodes);
+    var user = UserStore.getCurrentUser();
+    actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
+        type: "CONNECTION_OPEN", user: {flags: user.flags |= value}, experiments: [],
+    });
+    actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
+    webpackChunkdiscord_app.pop(); user.flags &= ~value; "done";
+}
+
+module.exports = class DiscordExperiments {
+	start() {
+        setExperiments(1)
+	}
+
+	stop() {
+        setExperiments(0)
+	}
+}
